1966a57a63c57c543925ccb978c818292a74e065
nicolas.guelfi <nicolas.guelfi@uni.lu>
2015-10-19 16:57:24 +0200

    1st

---
+++ test/src/add.h
@@ -0,0 +1,13 @@
+/*
+ * add.h
+ *
+ *  Created on: Oct 18, 2015
+ *      Author: nicolas.guelfi
+ */
+
+#ifndef ADD_H_
+#define ADD_H_
+
+int add(int x, int y);
+
+#endif /* ADD_H_ */
---
+++ test/src/test.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : NG
+// Version     :
+// Copyright   : secret
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "add.h"
+using namespace std;
+
+int main() {
+	int i ;
+	i = add(3,4);
+	cout << "Nicolas" << endl; // prints Nicolas
+	cout << i ;
+	return 0;
+}
